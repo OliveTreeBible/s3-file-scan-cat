6ceefc3b582fe3c3bf61dde2f3fdae78ad39d73f
David Trotz <davidtrotz@olivetree.com>
2020-11-19 19:30:04 -0800

    Minor formatting changes

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,13 +1,11 @@
-import {
-    LFService, LoggerFactory, LoggerFactoryOptions, LogGroupRule, LogLevel
-} from 'typescript-logging'
+import { LFService, LoggerFactory, LoggerFactoryOptions, LogGroupRule, LogLevel } from 'typescript-logging'
 
 let loggerFactory: LoggerFactory
 
 export function createLogger(groupingRule: string, logLevel: LogLevel, loggerName: string) {
     const options = new LoggerFactoryOptions()
     options.addLogGroupRule(new LogGroupRule(new RegExp(groupingRule), logLevel))
-    if(loggerFactory === undefined) {
+    if (loggerFactory === undefined) {
         loggerFactory = LFService.createNamedLoggerFactory('s3fsc-logger-factory', options)
     }
     return loggerFactory.getLogger(loggerName)
